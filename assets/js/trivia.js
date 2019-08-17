3d87c97448f2e4e0d26f9f69a8231119880c53f6
osmosismediainc <osmosismediainc@gmail.com>
2019-08-17 10:13:09 -0400

    screen changes

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,97 +1,66 @@
-/*
-_____________________ Plan ______________________
-On the landing page there will be a simple header and button.
-Upon the submit event of the button, the quiz will start.
-#timerDigits DIV will display a timer.
-#gameArea div will contain 2 nested for loops.
-Loop 1 for the length of the array of questions
-    write the html for the questions to the page
-        Loop 2 for the length of the radio buttons
-            $write the html of the buttons to the page
-At the end of the loop $write a submit button
-The game will end upon the countdown = 0 or if user hits submit.
-$write the results to the html page
-Provide option to reset the game and start over.
-3 variables to
-1) Right 
-2) Wrong 
-3) Unanswered 
-4) Hold the user selection
-An object 
-Question 1 array
-Radio Buttons 2 Array
-Right Answer
-*/
-
 // Doc Ready
 $(document).ready(function () {
+    // Hide The Quiz Area
+    $('#quizArea').hide();
+    $('#results').hide();
+    $('#restart').click(function() {
+        location.reload();
+    });
+
     // Variables for player
     let right = 0;
     let wrong = 0;
     let unanswered = 12;
     let answer = "";
-    // Object for questions
-    let quiz = {
-        questions: [
-            'The Great Wall of China is visible from the moon.',
-            'Scotland voted to become an independent country during the referendum from September 2014.',
-            'Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.',
-            'Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian.',
-            'The Lego Group was founded in 1932.',
-            'When you cry in space, your tears stick to your face.',
-            'On average, at least 1 person is killed by a drunk driver in the United States every hour.',
-            'You can legally drink alcohol while driving in Mississippi.',
-            'The Sun rises from the North',
-            'It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.',
-            'The color orange is named after the fruit.',
-            'French is an official language in Canada.',
-        ],
-        choices: ['False', 'True'
-        ],
-        answers: ['False',
-            'True',
-            'False',
-            'True',
-            'True',
-            'True',
-            'True',
-            'True',
-            'False',
-            'False',
-            'True'
-
-        ],
-    }
-
-    // Display Questions & Answers
-    // Loop de dupe for questions
-    for (var i = 0; i < quiz.length; i++) {
-        console.log(quiz.questions[i]);
-        // 
-        // $(askQuestion).text(quiz.question[i]);
-
-        // 
-    }
-
 /*
--------------------START---------------
-*/
+    // Arays for questions
+    let questions = ['The Great Wall of China is visible from the moon.',
+        'Scotland voted to become an independent country during the referendum from September 2014.',
+        'Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.',
+        'Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian.',
+        'The Lego Group was founded in 1932.',
+        'When you cry in space, your tears stick to your face.',
+        'On average, at least 1 person is killed by a drunk driver in the United States every hour.',
+        'You can legally drink alcohol while driving in Mississippi.',
+        'The Sun rises from the North',
+        'It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.',
+        'The color orange is named after the fruit.',
+        'French is an official language in Canada.'
+    ];
+    */
+    let answers = ["False",
+        'False',
+        'True',
+        'True',
+        'True',
+        'True'
+    ];
 
 
-/* Gets Start Button
-
-$( "#start" ).click(function() {
-    alert( "start" );
-  });
-*/
-
-/*       //  Start on click.
-      $("#start").on("click", function() {
-        //  Set the button alert's timeout to run three seconds after the function's called.
-        delayButtonAlert = setTimeout(function() {
-          alert("Alert #2: Called 3 seconds after the start button is clicked.");
-        }, 3000);
-      });
-
-      */
+    function displayQuestions() {
+        $('#quizArea').show();
+    }
+    //  Start on click.
+    $("#start").on("click", function () {
+        // Hide the start button
+        $("#start").hide();
+        // Show The Quiz Area
+        displayQuestions();
+        var count = 5;
+        var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+        function timer() {
+            count = count - 1;
+            if (count < 0) {
+                clearInterval(counter);
+                //counter ended, do something here // Show the total hide the gameDiv
+                $('#quizArea').hide();
+                $('#results').show();
+                $('#timerRow').hide();
+                $('#instructions').hide();
+                $('#start').hide();
+                return;
+            }
+            $("#timerDigits").text(count);
+        }
+    });
 });
