c300d1e5cd359729e896ae963d5bc3ce2bc209bc
osmosismediainc <osmosismediainc@gmail.com>
2019-08-17 20:27:30 -0400

    tweaks

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -135,7 +135,7 @@ $(document).ready(function () {
         $("#start").hide();
         // Show The Quiz Area
         displayQuestions();
-        var count = 60;
+        var count = 15;
         var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
         function timer() {
             count = count - 1;
